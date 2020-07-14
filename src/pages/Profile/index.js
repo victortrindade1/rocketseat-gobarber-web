d589356b7a0b5c3cc17291c2d1bc532aaa364e30
Victor <victortrindade@gmail.com>
2020-07-14 11:43:31 -0300

    Pagina de perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from 'unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      {/* initialData é um argumento pronto da lib. Faz com q ao carregar a
+      página, já aparecerem os dados preenchidos no form */}
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input
+          name="oldPassword"
+          type="password"
+          placeholder="Sua senha atual"
+        />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação da senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
 
 export default Profile;
